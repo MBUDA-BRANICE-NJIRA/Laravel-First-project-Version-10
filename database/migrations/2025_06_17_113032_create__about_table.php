4f9dc20c018647f1dd10cc3354ea5f9909e2fa12
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-06-17 14:51:29 +0300

    Worked on migration and instead of using mysql i used sqlite

--- database/migrations/2025_06_17_113032_create__about_table.php
+++ database/migrations/2025_06_17_113032_create__about_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('_about', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable();
+            $table->text('description');
+            $table->string('image');
             $table->timestamps();
         });
     }
