d635b4f734bc83857d3006889f2d7a5f41b7b943
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-06-17 15:12:16 +0300

    Learnt on the seeder

---
+++ database/seeders/AboutSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AboutSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Event::factorty()->count(40)->create();
+    }
+}
