00263932c5b7c7c226d46a139d367e73552dc7cc
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-06-18 14:27:48 +0300

    Started working on the consuming the fake generated data  by connecting the model on the about controller

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -8,6 +8,8 @@ class AboutController extends Controller
 {
     public function index()
     {
+
+        $events = About::all();// Assuming you have an About model to fetch data from the database
         return view('About');
     }
 }
